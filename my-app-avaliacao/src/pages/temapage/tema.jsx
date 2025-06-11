055852dad3c332edc867e1353aa087ce2434a5bb
Enzo Dias <lenzo7085@gmail.com>
2025-06-11 01:08:08 -0300

    fazendo troca de tema

--- my-app-avaliacao/src/pages/temapage/tema.jsx
+++ my-app-avaliacao/src/pages/temapage/tema.jsx
@@ -1,18 +1,26 @@
-import { Button } from "../../components/Button/button"
 import { Navbar } from "../../components/NavBar/navbar"
 import styles from "./tema.module.css"
+import { useState } from "react";
 
 export function Tema(){
+    const [tema, setTema] = useState("Tema claro")
+    const [mudarTema, setMudarTema] = useState("")
+
+    function handletema () {
+        if(tema === "Tema claro"){
+            setMudarTema("backgroudColor: white;")
+            setTema("Tema escuro")
+        }else if(tema === "Tema escuro"){
+
+            setTema("Tema Claro")
+        }
+    }
+
     return(
         <>
             <Navbar/>
-            <div className={styles.fantasmaTema}></div>
-            <div className={styles.buttonEditavel}>
-                <Button
-                    type="submit"
-                    onClick={""}
-                    title={'Teste'}
-                />
+            <div className={styles.body}>
+                
             </div>
         </>
     )
